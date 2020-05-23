e92a5ba552123d8cf5b3d51e7310c1117e5daff2
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-23 23:56:00 +0200

    added footer

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,16 @@ const App = () => {
     const showcase = React.createElement(Showcase);
     const infoSection = React.createElement(InfoSection);
     const featuresSection = React.createElement(FeaturesSection);
+    const clearFloats = React.createElement('div',{className: 'clr'});
+    const footer = React.createElement(Footer);
     return React.createElement(
         'div',
         {},
         header,
         showcase,
         infoSection,
-        featuresSection
+        featuresSection,
+        clearFloats,
+        footer
     );
 };
---
+++ scripts/components/footer.component.js
@@ -0,0 +1,11 @@
+
+const Footer = () => React.createElement(
+    'footer',
+    {id: 'main-footer'},
+    React.createElement(
+        'p',
+        {},
+        'Hotel DP \u00a9 2020 All Rights Reserved'
+    )
+);
+
