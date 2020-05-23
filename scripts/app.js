f775c5a96945d8915ac59663969bc3fd46837566
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-23 13:56:46 +0200

    moved Appo component in its own file

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+const App = () => {
+    const header = React.createElement(Header);
+    return React.createElement('div', null, header);
+};
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
-
-const App = () => {
-    const header = React.createElement(Header);
-    return React.createElement('div', null, header);
-};
-
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
         React.createElement(App),
