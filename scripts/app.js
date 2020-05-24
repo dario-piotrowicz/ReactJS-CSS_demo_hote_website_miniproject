91ad246e5176b84817d4a06a0612f1555e3fb87b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-24 20:26:24 +0200

    made app component a bit more JSX-like

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,33 @@
-const App = () => {
-
-    const routerSwitch = React.createElement(
-        ReactRouterDOM.Switch,
-        {},
-        React.createElement(
-            ReactRouterDOM.Route,
-            { exact: true, path: ['/index.html','/'] },
-            React.createElement(Home)
-        ),
-        React.createElement(
-            ReactRouterDOM.Route,
-            { exact: true, path:'/about'},
-            React.createElement(About)
-        ),
-        React.createElement(
-            ReactRouterDOM.Route,
-            { exact: true, path: '/contact'},
-            React.createElement(Contact)
-        )
-    );
-
-    const router = React.createElement(
-        ReactRouterDOM.BrowserRouter,
-        {},
-        React.createElement(
-            'div',
-            {},
-            React.createElement(ReactRouterDOM.withRouter(Header)),
-            routerSwitch
-        )
-    );
-
-    return React.createElement(
+const App = () => React.createElement(
         'div',
         {},
-        router,
+        React.createElement(
+            ReactRouterDOM.BrowserRouter,
+            {},
+            React.createElement(
+                'div',
+                {},
+                React.createElement(ReactRouterDOM.withRouter(Header)),
+                React.createElement(
+                    ReactRouterDOM.Switch,
+                    {},
+                    React.createElement(
+                        ReactRouterDOM.Route,
+                        {exact: true, path: ['/index.html','/']},
+                        React.createElement(Home)
+                    ),
+                    React.createElement(
+                        ReactRouterDOM.Route,
+                        {exact: true, path:'/about'},
+                        React.createElement(About)
+                    ),
+                    React.createElement(
+                        ReactRouterDOM.Route,
+                        {exact: true, path: '/contact'},
+                        React.createElement(Contact)
+                    )
+                )
+            )
+        ),
         React.createElement(Footer)
-    );
-};
+);
