3e13feed82d53b1829308947c63ec7bccc821080
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-24 20:15:14 +0200

    made home-info-section component a bit more JSX-like

--- scripts/components/home-info-section.component.js
+++ scripts/components/home-info-section.component.js
@@ -1,40 +1,38 @@
+const homeInfoDescriptionText = `
+    The Hotel does not actually exist, and DP stands
+    for Dario Piotrowicz which is me the developer
+    who is making this miniproject website.
+`;
 
-const HomeInfoSection = () => {
-
-    const descriptionText = `
-     The Hotel does not actually exist, and DP stands
-     for Dario Piotrowicz which is me the developer
-     who is making this miniproject website.
-    `;
-
-    const img = React.createElement('div',{className: 'info-img'});
-
-    const title = React.createElement('h2',{},
-        React.createElement('span',{className: 'text-primary'},'The History'),
-        ' Of Our Hotel'
-    );
-
-    const description = React.createElement('p',{},descriptionText);
-
-    const readMoreButton = React.createElement(
-        ReactRouterDOM.Link,
-        {to: '/about', className: 'btn btn-light'},
-        'Read More'
-    );
-
-    const content = React.createElement(
-        'div',
-        {className: 'info-content'},
-        title,
-        description,
-        readMoreButton
-    );
-
-
-    return React.createElement(
+const HomeInfoSection = () => React.createElement(
         'section',
-        { id: "home-info-section", className: "background-dark" },
-        img,
-        content
-    );
-};
+        {id: "home-info-section", className: "background-dark"},
+        React.createElement(
+            'div',
+            {className: 'info-img'}
+        ),
+        React.createElement(
+            'div',
+            {className: 'info-content'},
+            React.createElement(
+                'h2',
+                {},
+                React.createElement(
+                    'span',
+                    {className: 'text-primary'},
+                    'The History'
+                ),
+                ' Of Our Hotel'
+            ),
+            React.createElement(
+                'p',
+                {},
+                homeInfoDescriptionText
+            ),
+            React.createElement(
+                ReactRouterDOM.Link,
+                {to: '/about', className: 'btn btn-light'},
+                'Read More'
+            )
+        )
+);
