2e303ee868cb8d479531df02556df301ec27b1c4
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-27 21:30:18 +0200

    put comment in index.js for basename

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
+const basename = '/ReactJS-CSS_demo_hotel_miniproject';
+//Set basename to '/' for local development
+
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
         React.createElement(
             ReactRouterDOM.BrowserRouter,
-            { basename: '/ReactJS-CSS_demo_hotel_miniproject' },
+            { basename },
             React.createElement(ReactRouterDOM.withRouter(App))
         ),
         document.getElementById('root')
