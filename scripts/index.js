131e45e7a2689999f7304e7ed051db782b35cf81
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-27 21:06:10 +0200

    setting basename (in order to debug gh-pages deployment)

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log({ basename:process.env.PUBLIC_URL });
     ReactDOM.render(
         React.createElement(
             ReactRouterDOM.BrowserRouter,
-            {},
+            { basename: process.env.PUBLIC_URL },
             React.createElement(ReactRouterDOM.withRouter(App))
         ),
         document.getElementById('root')
