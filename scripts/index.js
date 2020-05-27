91837bd410a1ef08072c18cb00b43511f8ad7617
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-27 21:15:32 +0200

    added missing space

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
         React.createElement(
             ReactRouterDOM.BrowserRouter,
-            {basename: '/ReactJS-CSS_demo_hotel_miniproject' },
+            { basename: '/ReactJS-CSS_demo_hotel_miniproject' },
             React.createElement(ReactRouterDOM.withRouter(App))
         ),
         document.getElementById('root')
