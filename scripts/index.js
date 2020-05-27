a2be2496277c622271e2fb57e93f1652ffc29cfa
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-27 21:09:51 +0200

    setting hardcoded basename (in order to debug gh-pages deployment)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log({ basename:process.env.PUBLIC_URL });
     ReactDOM.render(
         React.createElement(
             ReactRouterDOM.BrowserRouter,
-            { basename: process.env.PUBLIC_URL },
+            {basename: '/ReactJS-CSS_demo_hotel_miniproject' },
             React.createElement(ReactRouterDOM.withRouter(App))
         ),
         document.getElementById('root')
