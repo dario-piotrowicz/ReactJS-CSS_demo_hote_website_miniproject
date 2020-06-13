92cf754117cfeb72ec8ff1a646b0a3aac7ba8cef
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-13 18:22:13 +0200

    using HashRouter for gh-pages deployment

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,8 @@
-const basename = '/ReactJS-CSS_demo_hotel_miniproject';
-//Set basename to '/' for local development
-
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
         React.createElement(
-            ReactRouterDOM.BrowserRouter,
-            { basename },
+            ReactRouterDOM.HashRouter,
+            { basename: '/' },
             React.createElement(ReactRouterDOM.withRouter(App))
         ),
         document.getElementById('root')
